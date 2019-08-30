9a9dbcd95e2b8e10287797bd768014ab3d842e99
Danny Ryan <dannyjryan@gmail.com>
2019-08-30 15:04:18 -0600

    update mocked start keygen to output 0x bytes

--- interop/mocked_start/keygen.py
+++ interop/mocked_start/keygen.py
@@ -1,9 +1,20 @@
 from py_ecc.bls.api import privtopub
 from hashlib import sha256 as _sha256
 from typing import List, Dict
-from yaml import dump, Dumper
+from ruamel.yaml import YAML
+from eth_utils import (
+    encode_hex,
+    int_to_big_endian,
+)
 
 CURVE_ORDER = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+
+
+def int_to_hex(n: int, byte_length: int = None) -> str:
+    byte_value = int_to_big_endian(n)
+    if byte_length:
+        byte_value = byte_value.rjust(byte_length, b'\x00')
+    return encode_hex(byte_value)
 
 
 def sha256(x):
@@ -15,15 +26,19 @@ def generate_validator_keypairs(N: int) -> List[Dict]:
     for index in range(N):
         privkey = int.from_bytes(
             sha256(index.to_bytes(length=32, byteorder='little')),
-            byteorder='little'
+            byteorder='little',
         ) % CURVE_ORDER
-        pubkey = privtopub(privkey)
-        keypairs.append({'privkey': privkey, 'pubkey': pubkey})
+        keypairs.append({
+            'privkey': int_to_hex(privkey),
+            'pubkey': '0x' + privtopub(privkey).hex(),
+        })
     return keypairs
 
 
 if __name__ == '__main__':
+    yaml = YAML(pure=True)
+    yaml.default_flow_style = None
+
     keypairs = generate_validator_keypairs(10)
-    keypairs_yaml = dump(keypairs, Dumper=Dumper)
-    with open('keygen_test_vector.yaml', 'w') as f:
-        f.write(keypairs_yaml)
+    with open('keygen_10_validators.yaml', 'w') as f:
+        yaml.dump(keypairs, f)
